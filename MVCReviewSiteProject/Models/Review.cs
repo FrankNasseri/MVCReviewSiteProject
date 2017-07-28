f1aba28f65d1bf74affa2b4e332b38633673eaf4
Frank Nasseri <franknasseri57@gmail.com>
2017-07-28 11:56:23 -0400

    added models

---
+++ MVCReviewSiteProject/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCReviewSiteProject.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string YesOrNO { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
